76e7859132e2cde65505d0ffe482f5e41d6155b2
Abhishek Kumar <abhishekk580@gmail.com>
2023-09-24 23:07:45 +0530

    Added Navbar and daisy ui

---
+++ app/components/NavBar/NavBar.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+const NavBar = () => {
+  return (
+    <div className="navbar bg-base-100 border-b-4 border-indigo-500 shadow-lg">
+      <div className="flex-1">
+        <Image
+          src="https://cdn.procoding.org/assets/procoding.png"
+          width={50}
+          height={50}
+          alt="ProCoding.org"
+        />
+        <a className="btn btn-ghost normal-case text-xl px-0">ProCoding</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>C Programs</a>
+          </li>
+          <li>
+            <a>Python</a>
+          </li>
+          <li>
+            <a>Django</a>
+          </li>
+          <li>
+            <a>Data Structures</a>
+          </li>
+          <li>
+            <a>Blog</a>
+          </li>
+          <li>
+            <a>Contact us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.procoding.org",
+        port: "",
+        pathname: "/assets/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
